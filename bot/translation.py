251355ac9d315b890ad88c798a82cd9145f35420
universitywits <84776816+universitywits@users.noreply.github.com>
2021-08-07 23:39:19 +0530

    Add files via upload

---
+++ bot/translation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+class Translation(object):
+    
+    START_TEXT = """<b>Hey {}!!</b>
+<i>നീ ഏതാ മോനൂസെ എന്നെ 𝗠𝗼𝘃𝗶𝗲 <a href="https://t.me/moviesmediagroup" >𝗠𝗼𝘃𝗶𝗲 𝗠𝗲𝗱𝗶𝗮 𝗚𝗿𝗼𝘂𝗽</a> ഗ്രൂപ്പിൽ മാത്രമേ ഉപയോഗിക്കാൻ പറ്റൂ...
+
+@rahulrajesh09 ആണ് എന്റെ മുതലാളി. 🤭
+</i>"""    
+    
+    HELP_TEXT = """
+<b>I can't help you contact my admin</b>
+"""
+
+    
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    
+<b>➥ Creator</b> : <b><i><a href="http://t.me/cw_perfect">perfect_bots</a></i></b>
+
+<b>➥ Language</b> : <code>Python3</code>
+
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+"""
